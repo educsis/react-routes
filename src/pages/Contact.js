7fb0d9c2c4ddaf637ce5847434d89ebc4e958f36
Edwin Orellana <educsis@gmail.com>
2023-04-25 17:16:11 -0600

    Finished react routes tutorial

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,15 @@
+import { useLocation } from "react-router-dom"
+
 export default function Contact() {
+    const queryString = useLocation().search
+    const queryParams = new URLSearchParams(queryString)
+    const name = queryParams.get("name")
+
     return  (
         <div>
             <h2>Contact</h2>
             <p>lorem lorem lorem</p>
+            <h1>{name}</h1>
         </div>
     )
 }
